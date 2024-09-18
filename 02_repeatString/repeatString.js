a8b72d6d21c89ff052af6aaf30e45f4c47d7b125
LorenzoS-93 <180427301+LorenzoS-93@users.noreply.github.com>
2024-09-18 18:35:06 +0200

    Exercise 1-2 done Exercise 1-2 done, remember you can't modify string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let aux = "";
+    if (num < 0) return 'ERROR';
+    for(let i =1; i <= num; i++)
+        aux+=str;
+    return aux;
 };
 
 // Do not edit below this line
